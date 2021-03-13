b1855d1dc3b70a78d8f24fc02c2f4c44d8ac09bd
DGangl <d.gangl@firestart.com>
2021-03-13 08:31:41 +0100

    chore: remove console logs

--- content.js
+++ content.js
@@ -1,10 +1,7 @@
 chrome.runtime.onMessage.addListener((request) => {
     const scope = document.getElementById('search');
-    console.log(scope);
     const searchresultLinks = scope.getElementsByTagName('a');
-    console.log(searchresultLinks);
     const requestedLink = searchresultLinks.item(request.linkIndex).childNodes[0];
-    console.log(requestedLink);
 
     requestedLink.click();
 });
